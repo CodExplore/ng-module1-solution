61641b553ac9f1fe39f59cf0475732ee748c70af
unknown <azonranker@yahoo.com>
2016-12-11 16:06:03 +0530

    Assignment 1

---
+++ app.js
@@ -0,0 +1,32 @@
+(function () {
+	'use strict';
+	angular.module('LunchCheck', []).controller('LunchCheckController', LunchCheckController);
+
+	LunchCheckController.$inject = ['$scope'];
+
+	function LunchCheckController($scope) {
+
+
+		$scope.LunchTest = function () {
+			$scope.messageArea = "Please enter data first";
+			var contentList = $scope.itemsList;
+			var splitList = contentList.split(" ");
+
+			var countItems = 0;
+			for (var i = 0; i < splitList.length; i++) {
+				countItems = countItems + 1;
+			}
+
+			if (countItems <= 3 && countItems !== 0) {
+				$scope.messageArea = "Enjoy";
+			}
+			if (countItems > 3) {
+				$scope.messageArea = "Too Much";
+			}
+
+		}
+
+
+	}
+
+})();
