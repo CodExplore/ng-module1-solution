ecc3876bff99423894ec759ea35434fcf4c1dcdb
unknown <azonranker@yahoo.com>
2016-12-12 07:45:06 +0530

    Final Update

--- app.js
+++ app.js
@@ -9,25 +9,27 @@
 
 		$scope.LunchTest = function () {
 			$scope.messageArea = "Please enter data first";
+	if ($scope.itemsList !=="" || !$scope.itemsList === " ") {
 			var contentList = $scope.itemsList;
 			var splitList = contentList.split(",");
 
 			var countItems = 0;
 			for (var i = 0; i < splitList.length; i++) {
 				countItems = countItems + 1;
+				
 			}
 
-			if (countItems <= 3 && !countItems === 0) {
+			if (countItems > 0 && countItems <= 3) {
 				$scope.messageArea = "Enjoy";
 			}
 			if (countItems > 3) {
 				$scope.messageArea = "Too Much";
 			}
-
+			}
 		}
         $scope.CheckChange = function () {
 		if($scope.itemsList ==="") {
-			$scope.messageArea = "Please enter data first";
+			$scope.messageArea = " ";
 		}
 	}
 
